b809277c69a1e1d9bab8934f437c6af10cb09f5c
Nico <nico.bohlinger@gmail.com>
2020-10-15 18:52:52 +0200

    Fix for: unknown error: DevToolsActivePort file

--- scraper.py
+++ scraper.py
@@ -24,6 +24,7 @@ chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(
     executable_path=sys.argv[1],
